320eb1a330a79500d7271dcbf1df32d57f503176
Mila <milaprivalova1@gmail.com>
2022-08-14 11:44:07 +0300

    task13

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 100)
+{
+    int result = number / 100 % 10;
+
+    Console.Write($"Третья цифра числа {number}: {result}");
+
+}
+else
+{
+    Console.WriteLine($"в числе {number} третьей цифры нет");
+
+}
