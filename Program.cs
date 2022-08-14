5c47ccb37ab3b6f3120debad645dfdeebd0dd67e
Mila <milaprivalova1@gmail.com>
2022-08-14 12:51:45 +0300

    task15

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число от 1 до 7 обозначающий день недели ");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine($"{num} выходной");
+}
+else
+{
+    Console.WriteLine($"{num} рабочий день");
+}
