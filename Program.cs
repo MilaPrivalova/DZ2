f759ccb4dfd1c6295d7822ff68abc50c3359d247
Mila <milaprivalova1@gmail.com>
2022-08-19 15:22:05 +0300

    TASK21 решение

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿
-Console.WriteLine("Задача 21");
-int x1 = 1;
-int y1 = 1;
-int z1 = 1;
-int x2 = 0;
-int y2 = 0;
-int z2 = 0;
-double distanse = Math.Sqrt( Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+﻿/* Задача 21: Напишите программу, которая принимает 
+на вход координаты двух точек и находит расстояние между 
+ними в 3D пространстве.*/
+
+Console.Write("Ведите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Ведите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine()); 
+Console.Write("Ведите координаты c1 ");
+int c1 = int.Parse(Console.ReadLine());
+Console.Write("Ведите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Ведите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine()); 
+Console.Write("Ведите координаты c2: ");
+int c2 = int.Parse(Console.ReadLine()); 
+double distanse = Math.Sqrt( Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((c1 - c2), 2));
 Console.WriteLine(distanse);
