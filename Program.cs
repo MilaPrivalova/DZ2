e6a859207e1c5691a3c9e1eeeb363ad73b17bec8
Mila <milaprivalova1@gmail.com>
2022-08-19 14:30:17 +0300

    task21

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Задача 21");
+int x1 = 1;
+int y1 = 1;
+int z1 = 1;
+int x2 = 0;
+int y2 = 0;
+int z2 = 0;
+double distanse = Math.Sqrt( Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+Console.WriteLine(distanse);
