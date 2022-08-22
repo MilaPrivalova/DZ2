b8b4b58deb0258f6ecb95ef4480c63da5dc7b2fa
Mila <milaprivalova1@gmail.com>
2022-08-22 19:11:22 +0300

    TASK27

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите функцию и запустите ее, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum;
+int result = 0;
+
+while (num>0)
+{
+    sum = num%10;
+    num = num/10;
+    result = result+sum;
+}
+Console.WriteLine(result);
+
+
+
