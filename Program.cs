b8ba6cf18f825a033ea6acfc2aaf27b4341ac872
Mila <milaprivalova1@gmail.com>
2022-08-23 14:54:33 +0300

    TASK29

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов,
+ заполненных случайными числами из [a, b) и выводит их на экран.*/
+
+ int[] GetArray(int size, int minValue, int maxValue)
+
+{
+    int[] rec = new int[size];
+
+    for (int i = 0; i< size; i++)
+    {
+        rec[i] =  new Random().Next(minValue, maxValue + 1);
+    }
+    return rec;
+}
+
+int[] array = GetArray(3,1,35);
+Console.WriteLine(String.Join(" ", array));
