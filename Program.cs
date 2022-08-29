6c0f74c16011ed53256c6ceb17e08ea2e7d89b1c
Mila <milaprivalova1@gmail.com>
2022-08-29 14:10:13 +0300

    TASK41

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[m];
+
+void NumPositiv(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+   arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comp(int[] masNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < masNumbers.Length; i++)
+  {
+    if(masNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+NumPositiv(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comp(arrayNumbers)} ");
