785b7f1b3edc3be47b15843a9e4e1d7ef42fc2a4
Mila <milaprivalova1@gmail.com>
2022-08-29 15:25:47 +0300

    TASK43

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите числа k1 и b1 уравнения первой прямой через пробел");
+double[] line1 = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+Console.WriteLine("Введите числа k2 и b2 уравнения второй прямой через пробел");
+double[] line2 = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+
+
+void dot(double[] y1, double[] y2)
+{
+    double x= (y2[1] - y1[1]) / (y1[0] - y2[0]);
+    double y = y1[0] * x + y1[1];
+    Console.WriteLine($"Точка пересечения прямых ({x}, {y})");
+}
+
+//x = -(b1 - b2) / (k1 - k2); y = k1 * x + b1;
+//k1 * x + b1, y = k2 * x + b2
+ dot( line1,line2 );
+ 
